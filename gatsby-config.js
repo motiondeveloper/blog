a04bae54564868e6eba0ccd6211db2d866a0fcf5
timhaywood <tim@haywood.org>
2019-07-16 16:51:47 +1000

    fix manifest config (favicon)

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,13 +67,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Motion Developer Blog`,
+        short_name: `MotionDeveloper`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
+        background_color: `#282C35`,
+        theme_color: `#B4DC96`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/md-logo.png`,
       },
     },
     `gatsby-plugin-offline`,
