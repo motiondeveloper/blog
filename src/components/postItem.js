eaffab9a3fc42012741e9b1ddc227d879bb3e5cb
timhaywood <tim@haywood.org>
2019-11-06 07:43:58 +1100

    move post items into seperate component

---
+++ src/components/postItem.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Emoji from "../components/emoji"
+import { Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+
+function PostItem({post, isPinned}) {
+    const title = post.frontmatter.title || post.fields.slug
+    const prefixWithPin = (content) => {
+        return (
+            <>
+                <Emoji name="Pin" emoji="📌"/> {content}
+            </>
+        )
+    }
+    return (
+        <div key={post.fields.slug}>
+        <h3
+            style={{
+            marginBottom: 0,
+            }}
+        >
+            <Link style={{ boxShadow: `none` }} to={post.fields.slug}>
+                {isPinned ? prefixWithPin(title) : title}
+            </Link>
+        </h3>
+        <small>{post.frontmatter.date}</small>
+        <p
+            style={{
+                marginTop: rhythm(1/4),
+            }}
+            dangerouslySetInnerHTML={{
+                __html: post.frontmatter.description || post.excerpt,
+            }}
+        />
+        </div>
+    )
+}
+
+export default PostItem
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,28 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Emoji from "../components/emoji"
-import { rhythm } from "../utils/typography"
+import PostItem from "../components/postItem"
 
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
+    const pinnedPosts = posts.filter(({ node }) => node.frontmatter.pinned)
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
-        {posts.filter(({ node }) => node.frontmatter.pinned).map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: 0,
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  <Emoji name="Pin" emoji="📌"/> {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                style={{
-                  marginTop: rhythm(1/4),
-                }}
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
+        {pinnedPosts.map(({ node }) => {
+          return <PostItem post={node} isPinned={true}/>
         })}
         <hr/>
         {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: 0,
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                style={{
-                  marginTop: rhythm(1/4),
-                }}
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
+          return <PostItem post={node} isPinned={false}/>
         })}
       </Layout>
     )
