d61b19bcb26d9a7fa0cb2e833c628ac0d11bde05
timhaywood <tim@haywood.org>
2019-11-21 08:25:40 +1100

    feat: swap embed videos with local video chain

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,18 +30,42 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-videos`,
+              options: {
+                pipelines: [
+                  {
+                    name: 'vp9',
+                    transcode: chain =>
+                      chain
+                        .videoCodec('libvpx-vp9')
+                        .noAudio()
+                        .outputOptions(['-crf 20', '-b:v 0']),
+                    maxHeight: 480,
+                    maxWidth: 900,
+                    fileExtension: 'webm',
+                  },
+                  {
+                    name: 'h264',
+                    transcode: chain =>
+                      chain
+                        .videoCodec('libx264')
+                        .noAudio()
+                        .addOption('-profile:v', 'main')
+                        .addOption('-pix_fmt', 'yuv420p')
+                        .outputOptions(['-movflags faststart'])
+                        .videoBitrate('1000k'),
+                    maxHeight: 480,
+                    maxWidth: 900,
+                    fileExtension: 'mp4',
+                  },
+                ],
+              }
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
             },
-          },
-          {
-            resolve: `gatsby-remark-embed-video`,
-            options: {
-              width: 590,
-              related: false,
-              noIFrameBorder: true,
-            }
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
