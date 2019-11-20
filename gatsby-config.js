ff9069576d151dbe6c5fbe445fbf2218621d29ff
timhaywood <tim@haywood.org>
2019-11-21 08:17:06 +1100

    feat: add gatsby-remark-embed-video

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,14 @@ module.exports = {
             },
           },
           {
+            resolve: `gatsby-remark-embed-video`,
+            options: {
+              width: 590,
+              related: false,
+              noIFrameBorder: true,
+            }
+          },
+          {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
               wrapperStyle: `margin-bottom: 1.0725rem`,
