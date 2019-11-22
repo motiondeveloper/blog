b915f096a4fac5d928ad48a7cd19843dda04f7e5
timhaywood <tim@haywood.org>
2019-11-22 16:54:02 +1100

    feat: add custom blog header

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Bio from '../components/bio';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import PostItem from '../components/postItem';
+
+import { colors } from '../theme';
 
 class BlogIndex extends React.Component {
   render() {
@@ -12,11 +13,22 @@ class BlogIndex extends React.Component {
     const siteTitle = data.site.siteMetadata.title;
     const posts = data.allMarkdownRemark.edges;
     const pinnedPosts = posts.filter(({ node }) => node.frontmatter.pinned);
+    const { author, social } = data.site.siteMetadata;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
-        <Bio />
+        <hr />
+        <h1 style={{
+          color: colors.yellow
+        }}>Articles on Adobe After Effects, JavaScript, templates, and everything in between.</h1>
+        <p>
+          Connect with us on{' '}
+          <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
+          {` `}and{` `}
+          <a href={`https://github.com/${social.github}`}>Github.</a>
+      </p>
+        <hr />
         {pinnedPosts.map(({ node }) => {
           return <PostItem post={node} isPinned={true} key={node.fields.slug} />;
         })}
@@ -36,6 +48,10 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        social {
+          twitter
+          github
+        }
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
