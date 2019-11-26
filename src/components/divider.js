9f8adf0a7fb8186adf624f54dcaa0b8c8d01666f
timhaywood <tim@haywood.org>
2019-11-27 08:13:52 +1100

    style: fix divider color

--- src/components/divider.js
+++ src/components/divider.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import { colors, padding } from '../theme';
 
 const Divider = styled.hr`
+  border: 2px solid ${colors.black};
   margin-top: ${padding.large};
   margin-bottom: ${padding.large};
-  border-top: 2px solid ${colors.black};
 `;
+
 export default Divider;
