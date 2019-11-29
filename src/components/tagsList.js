165202d71741ccf312a4a0851ed2bc3242c6648a
timhaywood <tim@haywood.org>
2019-11-29 17:08:12 +1100

    style: add background to tags

--- src/components/tagsList.js
+++ src/components/tagsList.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { Link } from 'gatsby';
 import HorizontalList from './horizontalList';
 import styled from 'styled-components';
-import { padding } from '../theme';
+import { padding, colors } from '../theme';
+
+const tagColor = colors.green;
 
 const Tag = styled.li`
+  background-color: ${tagColor + `10`};
+  border-radius: ${padding.xxsmall};
+  padding: ${padding.xxsmall};
+  padding-top: 0;
+  padding-bottom: 0;
+  margin: -${padding.xxsmall};
+  :hover {
+    background-color: ${tagColor + `30`};
+  }
+  a {
+    color: ${tagColor};
+    text-decoration: none;
+  }
+  a :hover {
+    text-decoration: underline;
+  }
   &&& {
     :not(:last-child) {
       margin-right: ${padding.xsmall};
