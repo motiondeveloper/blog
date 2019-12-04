a70976380d4040d008ece72701959ae65cd5f899
timhaywood <tim@haywood.org>
2019-12-04 12:56:42 +1100

    style: remove highlight on hover

--- src/components/card.js
+++ src/components/card.js
@@ -10,10 +10,6 @@ const PostCard = styled.div`
   margin-right: -${padding.large};
   margin-bottom: ${padding.large};
   text-decoration: none;
-  transition: 0.2s ease;
-  :hover {
-    background-color: ${colors.black + '90'};
-  }
 `;
 
 export default PostCard;
