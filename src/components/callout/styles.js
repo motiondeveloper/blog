c17270458fa0ac1f12f354cf6c969fd1636dd3b2
Tim Haywood <tim@haywood.org>
2020-03-16 17:40:37 +1100

    fix:style: toc heading padding

--- src/components/callout/styles.js
+++ src/components/callout/styles.js
@@ -16,7 +16,8 @@ export const CalloutWrapper = styled.div`
 `;
 export const CalloutContent = styled.div`
   margin: 0;
-  p {
+  &&& ul,
+  p:last-child {
     margin: 0;
   }
 `;
@@ -28,9 +29,15 @@ export const CalloutIcon = styled.div`
 `;
 
 export const CalloutTitle = styled.div`
+  display: flex;
   position: absolute;
   top: -${padding.medium};
   box-shadow: 0 0 0 ${padding.xsmall} ${colors.navy};
   background: ${colors.navy};
   color: ${props => props.color};
+  &&& h2 {
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-left: ${padding.small};
+  }
 `;
