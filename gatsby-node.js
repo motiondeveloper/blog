84258402767ab89c4e5ccf31c5cbbee7d5f7cb3b
Tim Haywood <tim@haywood.org>
2020-05-28 18:11:50 +1000

    chore: formatting

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const tagTemplate = path.resolve(`src/templates/tags.js`)
+  const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  const tagTemplate = path.resolve(`src/templates/tags.js`);
   const result = await graphql(
     `
       {
@@ -30,62 +30,63 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-    `)
+    `,
+  );
 
-    if (result.errors) {
-      throw result.errors
-    }
+  if (result.errors) {
+    throw result.errors;
+  }
 
-    const posts = result.data.postsGroup.edges
-    const tags = result.data.tagsGroup.group
+  const posts = result.data.postsGroup.edges;
+  const tags = result.data.tagsGroup.group;
 
-    // Create post pages
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+  // Create post pages
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
 
-      createPage({
-        path: `/blog${post.node.fields.slug}`,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
+    createPage({
+      path: `/blog${post.node.fields.slug}`,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    });
+  });
 
-    // Create tag pages
-    tags.forEach(tag => {
-      createPage({
-        path: `/tags/${tag.fieldValue}/`,
-        component: tagTemplate,
-        context: {
-          tag: tag.fieldValue,
-        },
-      })
-    })
+  // Create tag pages
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${tag.fieldValue}/`,
+      component: tagTemplate,
+      context: {
+        tag: tag.fieldValue,
+      },
+    });
+  });
 
-    return null
-}
+  return null;
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
       value,
-    })
+    });
   }
-}
+};
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
-  })
-}
+  });
+};
