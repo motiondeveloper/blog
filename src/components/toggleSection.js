249a8e8abd940a04e18ce12cc79492e8f779aca2
Tim Haywood <tim@haywood.org>
2020-07-23 22:39:50 +1000

    feat: add toggleSection component

---
+++ src/components/toggleSection.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { colors, padding } from '../theme';
+import styled, { css } from 'styled-components';
+
+const Title = styled.h3``;
+
+const ToggleSection = ({ title, children, as = 'h3' }) => {
+  return (
+    <details
+      css={css`
+        summary::marker {
+          color: ${colors.blue};
+        }
+        summary::-webkit-details-marker {
+          color: ${colors.blue};
+        }
+      `}
+    >
+      <summary
+        css={css`
+          margin-bottom: ${padding.small};
+        `}
+      >
+        <Title
+          css={css`
+            display: inline;
+          `}
+          as={as}
+        >
+          {title}
+        </Title>
+      </summary>
+      {children}
+    </details>
+  );
+};
+
+export { ToggleSection };
